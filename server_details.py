b4e2cb34305838f9e3483e4ea05b426d85175618
ahmedrazarais <razarais28@gmail.com>
2024-04-14 17:41:04 +0500

    server-connection-details

---
+++ server_details.py
@@ -0,0 +1,6 @@
+# Setting server details
+db_host="example"     # replace wioth your host name
+db_user="example"                # replace with your user
+db_password=".........."        # replace with your password
+
+
